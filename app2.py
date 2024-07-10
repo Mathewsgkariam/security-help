62ca59879d36c07bdd7fe6502cfabb6855f80e9b
Mathewsgkariam <mathew_sg@me.com>
2024-07-10 22:11:02 +0800

    app2.py

--- app2.py
+++ app2.py
@@ -105,7 +105,7 @@ def display_response_animation(response, role):
 # You can define file paths and URLs
 file_paths = ['intents-responses-01.csv', 'data.txt','output.txt']
 urls = [
-    'https://www.channelnewsasia.com/topic/cybersecurity'
+    'https://www.channelnewsasia.com/topic/cybersecurity','https://sbr.com.sg/topics/singapore-cybersecurity'
     # You can add more URLs here
 ]
 
