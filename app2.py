3c839303067985e01163e3c687e6455c099b7f07
Mathewsgkariam <mathew_sg@me.com>
2024-07-10 21:53:03 +0800

    app2.py

--- app2.py
+++ app2.py
@@ -105,7 +105,6 @@ def display_response_animation(response, role):
 # You can define file paths and URLs
 file_paths = ['intents-responses-01.csv', 'data.txt','output.txt']
 urls = [
-    'https://www.selenium.dev/',
     'https://en.wikipedia.org/wiki/Demon'
     # You can add more URLs here
 ]
