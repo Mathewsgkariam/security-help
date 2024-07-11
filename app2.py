82661b30eee4b506096a22c7b8db1fa1a6c5a0b4
Mathewsgkariam <mathew_sg@me.com>
2024-07-11 09:36:16 +0800

    app2.py

--- app2.py
+++ app2.py
@@ -106,7 +106,7 @@ def display_response_animation(response, role):
 file_paths = ['intents-responses-01.csv', 'data.txt','output.txt','IMDA IoT Cyber Security Guide.pdf','Philips Hue 464487 Smart Bulb User Manual.pdf','isa-booklet.pdf']
 urls = [
     'https://www.channelnewsasia.com/topic/cybersecurity','https://sbr.com.sg/topics/singapore-cybersecurity','https://www.infosecurity-magazine.com/news/','https://thehackernews.com','https://www.tuvsud.com/en-sg/industries/consumer-products-and-retail/iot-cybersecurity/cybersecurity-labelling-scheme-from-cyber-security-agency-of-singapore',
-    'https://www.csa.gov.sg/our-programmes/certification-and-labelling-schemes/cybersecurity-labelling-scheme/cybersecurity-labelling-scheme-(cls)product-list'
+    'https://www.csa.gov.sg/our-programmes/certification-and-labelling-schemes/cybersecurity-labelling-scheme/cybersecurity-labelling-scheme-(cls)product-list','https://www.fortinet.com/resources/cyberglossary/iot-device-vulnerabilities'
     # You can add more URLs here
 ]
 
